6bff9bc93f0500f05d3e3d9959aad2faccb9e45d
alexmiretskiy <miretskiy@mail.ru>
2017-10-15 17:29:59 +0200

    ref

--- app/src/main/java/com/example/miret/booklisting/BookAdapter.java
+++ app/src/main/java/com/example/miret/booklisting/BookAdapter.java
@@ -1,13 +1,18 @@
 package com.example.miret.booklisting;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class BookAdapter extends ArrayAdapter<Book> {
@@ -24,23 +29,54 @@ public class BookAdapter extends ArrayAdapter<Book> {
       listItemView = LayoutInflater.from(getContext())
           .inflate(R.layout.book_list_item, parent, false);
     }
-      Book currentBook = getItem(position);
+    Book currentBook = getItem(position);
 
-      TextView titleTextView = (TextView) listItemView.findViewById(R.id.title_text_view);
-      titleTextView.setText(String.format("%s.", currentBook.getTitle()));
+    TextView titleTextView = (TextView) listItemView.findViewById(R.id.title_text_view);
+    titleTextView.setText(String.format("%s.", currentBook.getTitle()));
 
-      TextView authorsTextView = (TextView) listItemView.findViewById(R.id.author_text_view);
-      authorsTextView.setText(getAuthorsString(currentBook.getAuthors()));
+    TextView authorsTextView = (TextView) listItemView.findViewById(R.id.author_text_view);
+    authorsTextView.setText(getAuthorsString(currentBook.getAuthors()));
 
-      ImageView posterImageView = (ImageView) listItemView.findViewById(R.id.poster_image_view);
-      if (currentBook.getPoster() != null) {
-        Picasso.with(getContext()).load(currentBook.getPoster()).into(posterImageView);
-      }
+    ImageView posterImageView = (ImageView) listItemView.findViewById(R.id.poster_image_view);
+
+    new DownloadImageTask(posterImageView).execute(currentBook.getPoster());
+//    if (currentBook.getPoster() != null) {
+//      Picasso.with(getContext()).load(currentBook.getPoster()).into(posterImageView);
+//    }
+
 
     return listItemView;
   }
 
-  String getAuthorsString(String[] authorsArray) {
+  private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+    ImageView thumbnailImageView;
+
+    public DownloadImageTask(ImageView thumbnailImageView) {
+      this.thumbnailImageView = thumbnailImageView;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... urls) {
+      String urlDisplay = urls[0];
+      Bitmap bitmapImage = null;
+
+      try {
+        InputStream inputStream = new java.net.URL(urlDisplay).openStream();
+        bitmapImage = BitmapFactory.decodeStream(inputStream);
+      } catch (IOException e) {
+        Log.e(BookAdapter.class.getSimpleName(), "Image transmission error", e);
+      }
+      return bitmapImage;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+      thumbnailImageView.setImageBitmap(result);
+    }
+  }
+
+  private String getAuthorsString(String[] authorsArray) {
     StringBuilder output = new StringBuilder();
     String[] authors = authorsArray;
     for (int i = 0; i < authors.length; i++) {
